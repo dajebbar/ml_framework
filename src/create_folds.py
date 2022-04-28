2f4d01330982c24beb2f45a27457bb63a637a8fe
Dajebbar <dajebbar@gmx.fr>
2022-04-28 11:56:50 +0000

    folds

---
+++ src/create_folds.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import pandas as pd 
+from sklearn import model_selection
+
+if __name__ == '__main__':
+    df = pd.read_csv('input/train.csv')
+    df['kfold'] = -1
+
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    kf = model_selection.StatifiedKFold(n_split=5)
+    y = df.target.values
+
+    for f_, (t_, v_) in kf.split(X=df, y=y):
+        print(len(t_), len(v_))
+        df.loc[v_, 'kfold'] = f_
+    
+    df.to_csv('input/train_folds.csv', index=False)
